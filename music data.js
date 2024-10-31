f8c34b6bcfa4281b0009a6eb157b894af5ca780d
Olive Njoroge <njorogeolive@gmail.com>
2024-10-31 17:05:15 +0300

    Added js, more css and it's still not working😭😭😭

--- music data.js
+++ music data.js
@@ -1,8 +1,8 @@
 let songs = [{
-    "name" : "song 1",
-    "path" : "musics/Song 1.mp3",
-    "artist" : "artist 1",
-    "cover" : "images/cover 1.png"
+    "name" : "A Whole New World ",
+    "path" : "c:\\Users\\Olive\\AppData\\Local\\Packages\\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\\TempState\\40098D4AF08A53198BCA329E718D901D\\WhatsApp Audio 2024-10-31 at 16.54.39_eb4de9ee.mp3",
+    "artist" : "Peabo Bryson and Regina Belle",
+    "cover" : "https://i.ibb.co/2t7bWWg/baby-blue-color-solid-background-1920x1080.png"
 },
 {
     "name" : "song 2",
